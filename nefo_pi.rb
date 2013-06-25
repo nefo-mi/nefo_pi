ba303104407cec46f32a8ff48903449b411d588f
nefo_mi <nefo.mi@gmail.com>
2013-06-25 22:41:39 +0900

    add function that reply ip address.

--- nefo_pi.rb
+++ nefo_pi.rb
@@ -7,7 +7,7 @@ require 'user_stream'
 require 'twitter'
 
 def ip
-  `/sbin/ifconfig -a | grep -w inet |grep -v 127 | cut -d ' ' -f2`
+  `/sbin/ifconfig -a | grep -w inet |grep "192\|172" | cut -d ' ' -f2`
 end
  
 config_path = File.expand_path(File.dirname(__FILE__) + '/config.yml')
@@ -20,8 +20,24 @@ UserStream.configure do |c|
   c.oauth_token_secret = config.oauth_token_secret
 end
 
+Twitter.configure do |c|
+  c.consumer_key = config.consumer_key
+  c.consumer_secret = config.consumer_secret
+  c.oauth_token = config.oauth_token
+  c.oauth_token_secret = config.oauth_token_secret
+end
+
 client = UserStream.client
 
 client.user do |status|
-  puts status.text
+  if config.name == status.in_reply_to_screen_name
+    unless status.user.nil?
+      if config.targets.include? status.user.screen_name
+        case status.text
+        when %r(.*(ip|IP).*)
+          Twitter.update "@#{status.user.screen_name} ほらよ つ #{ip}"
+        end
+      end
+    end
+  end
 end
